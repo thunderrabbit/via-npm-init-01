fe5dfe9f83af14777d48b09ced940d14fe1800af
Ansible on Vagrant <ansible.on.vagrant@robnugen.com>
2022-09-16 10:01:35 +0000

    Create simple middleware

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ var app = express();
 const square = require("./middleware/square.js"); // .js file extension is optional
 const wiki = require("./middleware/wiki.js");  //  wiki.js will handle some routes
 
+// BEGIN example middleware function
+const a_middleware_function = function (req, res, next) {
+    console.log("in a_middleware_function");    // why won't logger work here?  it's defined.. hmmm
+    next(); // Call next() so Express will call the next middleware function in the chain.
+};
+// END example middleware function
+
 app.use(logger("dave_was_here"));  // write string into the same place as console.log
 app.use(logger('tiny'));    // e.g.  GET /wiki/square/4 304 - - 0.277 ms
+app.use(a_middleware_function);
 
 app.use("/wiki", wiki);   // tell wiki.js routes to be available from /wiki
 
