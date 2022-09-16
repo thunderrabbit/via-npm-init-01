b1ae248daf69d9fa8e3583dcdf4651fe971b4cb7
Ansible on Vagrant <ansible.on.vagrant@robnugen.com>
2022-09-16 09:32:58 +0000

    Almost allow wiki to do math

--- wiki.js
+++ wiki.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const router = express.Router();
+const square = require("./square.js"); // must be required in wiki.js even though already required in index.js
 
 // Home page route
 router.get("/", function (req, res) {
@@ -11,6 +12,12 @@ router.get("/", function (req, res) {
 // About page route
 router.get("/about", function (req, res) {
   res.send("About this wiki");
+});
+
+// Eventually send the parameter 4 into the function
+router.get('/square/4', function (req, res, next) {
+   res.send(`The area of a square with a width of 4 is ${square.area(4)}`);
+   next(); // pass control to the next handler
 });
 
 module.exports = router;
