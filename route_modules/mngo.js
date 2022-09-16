bc0bbf62e26a8c8411b7282f03791c56f1f3275a
Ansible on Vagrant <ansible.on.vagrant@robnugen.com>
2022-09-16 12:10:56 +0000

    Add mongo connect info sans PW
    from atlas.mongodb.com or something but now I cannot find the source of this source.
    
    Fortunately, in https://university.mongodb.com/mercury/M220JS/2022_September_13/chapter/Chapter_0_Introduction_and_Setup/lesson/5dfb70620ec5a4bbc5890878/lecture they talk about how to pur the key in a .env file

--- route_modules/mngo.js
+++ route_modules/mngo.js
@@ -1,5 +1,19 @@
 // mngo.js - Mngo route module so I can isolate the mongo import stuff while I'm playing with it
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://pigglywiggly:@cluster0.exfbgwt.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect(err => {
+  const collection = client.db("sample_mflix").collection("users");
+    // perform actions on the collection object
+    console.log(collection);
+  client.close();
+});
+
+
+
 const express = require("express");
 const router = express.Router();
 
