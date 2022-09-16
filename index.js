79c72676a049d2f2b88718b1b3089386aeb2531a
Ansible on Vagrant <ansible.on.vagrant@robnugen.com>
2022-09-16 09:28:37 +0000

    Add routes via simple wiki module

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 
 const square = require("./square.js"); // .js file extension is optional
+const wiki = require("./wiki.js");  //  wiki.js will handle some routes
+
+app.use("/wiki", wiki);   // tell wiki.js routes to be available from /wiki
 
 app.get('/', function (req, res, next) {
    res.send(`The area of a square with a width of 4 is ${square.area(4)}`);
