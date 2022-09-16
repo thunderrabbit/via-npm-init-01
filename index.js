f188e6021a313cc45ca6750e04423d25b080003b
Ansible on Vagrant <ansible.on.vagrant@robnugen.com>
2022-09-16 08:59:24 +0000

    Create a module 'square'

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+const square = require("./square"); // Here we require() the name of the file without the (optional) .js file extension
+
 app.get('/', function(req, res){
-   res.send("Hello world!");
+   res.send(`The area of a square with a width of 4 is ${square.area(4)}`);
 });
 
 app.listen(4000);
