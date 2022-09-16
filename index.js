b97dc19ab85c9b9580a4adf4b4bde99df2a06b1b
Ansible on Vagrant <ansible.on.vagrant@robnugen.com>
2022-09-16 09:15:05 +0000

    Create a /secret URL

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ var app = express();
 
 const square = require("./square"); // Here we require() the name of the file without the (optional) .js file extension
 
-app.get('/', function(req, res){
+app.get('/', function (req, res, next) {
    res.send(`The area of a square with a width of 4 is ${square.area(4)}`);
+   next(); // pass control to the next handler
 });
+
+app.all("/secret", function (req, res, next) {
+   res.send(`The area of a square with a width of 4.1 is ${square.area(4.1)}`);
+   next(); // pass control to the next handler
+});
+
 
 app.listen(4000);
